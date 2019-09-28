845d3b2c3c18bd7c7e32ec23779066e6c67ecc92
Andrew Spence <54445432+andrewspence2020@users.noreply.github.com>
2019-09-28 11:34:23 -0400

    Add files via upload

---
+++ csharp_homework_page456Q6/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Temperatures {
+    class Program {
+        static void Main(string[] args) {
+            var temps = new Temperatures(31.2, 25.5, 24.0, 34.3, 19.9, 23.8, 26.1);
+
+            Console.WriteLine("Weekly Temperatures:\n");
+            Console.WriteLine(temps);
+
+            Console.WriteLine($"\nAverage without lowest temperature is {temps.GetAverageWithoutLowest():f1}");
+        }
+    }
+}
---
+++ csharp_homework_page456Q6/Temperatures.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+
+namespace Temperatures {
+    class Temperatures {
+        double[] temps;
+
+        public Temperatures(params double[] temps) {
+            this.temps = temps;
+        }
+
+        public double GetHighest() {
+            return temps.Max();
+        }
+
+        public double GetLowest() {
+            return temps.Min();
+        }
+
+        public double GetAverage() {
+            return GetTotalOfTemps() / temps.Length;
+        }
+
+        public double GetAverageWithoutLowest() {
+            return (GetTotalOfTemps() - GetLowest()) / (temps.Length - 1);
+        }
+
+        public int GetNumberOfDaysUnder(double temp) {
+            int numberOfDays = 0;
+            foreach (double _temp in temps) {
+                if (temp > _temp) {
+                    numberOfDays++;
+                }
+            }
+            return numberOfDays;
+        }
+
+        private double GetTotalOfTemps() {
+            double total = 0;
+            foreach (double temp in temps) {
+                total += temp;
+            }
+            return total;
+        }
+
+        public override string ToString() {
+            return $"{"Minimum",-10} | {"Maximum",-10} | {"Average",-10}" +
+                $"\n{GetLowest(),-10:f1} | {GetHighest(),-10:f1} | {GetAverage(),-10:f1}";
+        }
+    }
+}
